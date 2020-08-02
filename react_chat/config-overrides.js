6dc282ec9368f554f63edf21bf16ea6bfce4b4a0
zhuchuanyong <zhuchuanyong@vtstar.net>
2020-08-02 14:41:57 +0800

    feat: 配置别名，全局引入scss

--- react_chat/config-overrides.js
+++ react_chat/config-overrides.js
@@ -1,5 +1,5 @@
-const { override, addPostcssPlugins } = require("customize-cra");
-
+const { override, addPostcssPlugins,addWebpackAlias, adjustStyleLoaders } = require("customize-cra");
+const path = require("path");
 module.exports = override(
     addPostcssPlugins(
         [
@@ -19,6 +19,25 @@ module.exports = override(
                 }
             )
         ]
-        
-    )
+    ),
+    addWebpackAlias({
+        "@src": path.resolve(__dirname, "src"),
+        "@image": path.resolve(__dirname, "src/image"),
+        "@style": path.resolve(__dirname, "src/style"),
+        "@utils": path.resolve(__dirname, "src/utils"),
+        "@components": path.resolve(__dirname, "src/components"),
+      }),
+      adjustStyleLoaders(rule => {
+        if (rule.test.toString().includes("scss")) {
+          rule.use.push({
+            loader: require.resolve("sass-resources-loader"),
+            options: {
+              resources: [
+                "src/style/variable.scss",
+                "src/style/mixins/index.scss",
+              ] //这里是你自己放公共scss变量的路径
+            }
+          });
+        }
+      })
   );
--- react_chat/src/views/home/index.tsx
+++ react_chat/src/views/home/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+
+import logo from '@image/home/logo@2x.png';
+import add from '@image/home/add@2x.png';
+import search from '@image/home/search@2x.png';
+import avatar from '../../image/avatar.png';
+import './index.scss';
 function Home() {
   return (
-    <div className="home">
-     首页
+    <div className="home content flex_center ">
+     <div className="top-bar">
+       <div className="top-bar-left">
+         <img src={avatar} alt="" />
+       </div>
+       <div className="top-bar-center">
+       <img src={logo} alt="" />
+       </div>
+       <div className="top-bar-right">
+       <img src={add} alt="" />
+       <img src={search} alt="" />
+       </div>
+     </div>
     </div>
   );
 }
