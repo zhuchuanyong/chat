48c43295d3b584ace8d85777ec5dff0fab67cbf7
zhuchuanyong <zhuchuanyong@vtstar.net>
2020-08-02 15:37:11 +0800

    feat: header

--- react_chat/src/views/home/index.tsx
+++ react_chat/src/views/home/index.tsx
@@ -6,22 +6,22 @@ import search from '@image/home/search@2x.png';
 import avatar from '../../image/avatar.png';
 import './index.scss';
 function Home() {
-  return (
-    <div className="home content flex_center ">
-     <div className="top-bar">
-       <div className="top-bar-left">
-         <img src={avatar} alt="" />
-       </div>
-       <div className="top-bar-center">
-       <img src={logo} alt="" />
-       </div>
-       <div className="top-bar-right">
-       <img src={add} alt="" />
-       <img src={search} alt="" />
-       </div>
-     </div>
-    </div>
-  );
+    return (
+        <div className="home content flex-center ">
+            <div className="top-bar flex1 flex-center-wrap">
+                <div className="top-bar-left flex-center ">
+                    <img src={avatar} alt="" />
+                </div>
+                <div className="top-bar-center flex-center flex1">
+                    <img src={logo} alt="" />
+                </div>
+                <div className="top-bar-right flex-center">
+                    <img className="pr-18" src={search} alt="" />
+                    <img src={add} alt="" />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
