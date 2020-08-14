c23786d6d44ce54eaf42a561d610f5ed34e75525
zhuchuanyong <zhuchuanyong@vtstar.net>
2020-08-14 23:14:31 +0800

    feat: 列表渲染

--- react_chat/src/views/home/components/chatList/index.tsx
+++ react_chat/src/views/home/components/chatList/index.tsx
@@ -1,29 +1,42 @@
-import React from 'react';
-import './index.scss';
-import avatar from '@src/image/avatar.png';
-import {chatListProps } from '@src/types';
+import React from "react";
+import "./index.scss";
+import avatar from "@src/image/avatar.png";
+// import {chatListProps } from '@src/types';
 // chatListProps
-function ChatList(props:chatListProps) {
-    console.log(props);
-    return (
-        <div className="chatList flex">
-           <div className="left">
-               <div className="tip flex-center">
-                   {props.num}
-               </div>
-               <img src={avatar} alt="" />
-           </div>
-           <div className="center overflowH flex1 pl-30">
-               <div className="top flex">
-                   <div className="name flex1">马化腾</div>
-                   <div className="time">11：42</div>
-               </div>
-               <div className="info ellipsis">
-               color: $text-color-disable;f我是谁是个打个电话
-               </div>
-           </div>
+export interface IProps {
+  num: number;
+  imgUrl?: string;
+  friendName?: string;
+  time?: string;
+  info?: string;
+}
+
+// 设置props 默认值
+ChatList.defaultProps = {
+  imgUrl: avatar,
+  friendName: "马化腾",
+  time: "11：42",
+  info:'天苍苍,野茫茫'
+};
+function ChatList(props: IProps) {
+  console.log(props);
+  return (
+    <div className="chatList flex">
+      <div className="left">
+        <div className="tip flex-center">{props.num}</div>
+        <img src={props.imgUrl} alt="" />
+      </div>
+      <div className="center overflowH flex1 pl-30">
+        <div className="top flex">
+          <div className="name flex1">{props.friendName}</div>
+          <div className="time">{props.time}</div>
         </div>
-    );
+        <div className="info ellipsis">
+            {props.info}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ChatList;
--- react_chat/src/views/home/index.tsx
+++ react_chat/src/views/home/index.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import HeaderCom from './components/Header';
-import ChatList from './components/chatList';
-import './index.scss';
+import HeaderCom from "./components/Header";
+import ChatList from "./components/chatList";
+import "./index.scss";
 function Home() {
-    return (
-        <div className="home h-min content ">
-            <HeaderCom></HeaderCom>
-            <div className="spacing-lf">
-                <ChatList  num={2}></ChatList>
-            </div>
-        </div>
-    );
+  let arr=Array(20).fill(2)
+
+
+  return (
+    <div className="home h-min content ">
+      <HeaderCom></HeaderCom>
+      <div className="spacing-lf">
+        <ChatList num={1} friendName="好友请求"></ChatList>
+       {
+         arr.map(()=>{
+        return   <ChatList num={2}></ChatList>
+         })
+       }
+
+      </div>
+    </div>
+  );
 }
 
 export default Home;
